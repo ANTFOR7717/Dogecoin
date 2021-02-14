1fb14ef828a924cb620941022ebff752e0949f30
ANTFOR7717 <anthonyforest21@gmail.com>
2021-02-14 12:09:22 -0500

    updated font load speed (drastic)

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'dogecoin',
+    title: 'Dogecoin | The Worlds Most Favored Currency',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
